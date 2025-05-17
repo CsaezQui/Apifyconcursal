b2d0c09cae3d677ec3122f993d663ad5c7fbd773
CsaezQui <carlos.saez.quintero@gmail.com>
2025-05-17 13:12:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { Actor } = require('apify');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 Actor.main(async () => {
     try {
@@ -8,14 +8,14 @@ Actor.main(async () => {
 
         console.log('Lanzando navegador...');
         const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/usr/bin/google-chrome-stable',
-    args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--window-size=1200,800'
-    ]
-});
+            headless: true,
+            executablePath: '/usr/bin/google-chrome',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--window-size=1200,800'
+            ]
+        });
 
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36');
